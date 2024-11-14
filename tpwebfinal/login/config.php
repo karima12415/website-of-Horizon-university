13c9571050fc8e352834feae00885d127db418fe
surayya <129226184+karima12415@users.noreply.github.com>
2024-11-14 18:42:03 +0000

    Delete tpwebfinal directory

--- tpwebfinal/login/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$com = mysqli_connect("localhost","root","","signup")or die("couldn't connect");
-?>
--- tpwebfinal/pages/addstudentfolder/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$id=$_GET['id'];
-	include('conn.php');
-	mysqli_query($conn,"DELETE  FROM student WHERE Id='$id'");
-	header('location: Ad-Dl-Student.php');
-?>
